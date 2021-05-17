e8292031b6bf268ae913f257d93596d818cb778b
Johannes Daub <polynoman@live.de>
2021-05-17 17:32:34 +0200

    Prepared repository for docker and flask.

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM from python:3.8-slim-buster
+
+RUN pip3 install --upgrade pip \
+    Flask==1.0.2 \
+    numpy==1.20.3 \
+
+
+EXPOSE 9657
+
+CMD [ "python3", "./starter.py" ]
---
+++ starter.py
@@ -0,0 +1,35 @@
+"""This file starts the microservice"""
+import os
+
+#!flask/bin/python
+from flask import Flask, json, jsonify, logging, request
+
+#import classification_facade
+
+with open('./config.json') as config_file:
+    CONFIG = json.load(config_file)
+
+app = Flask(__name__)
+
+
+@app.route("/hitec/classify/concepts/seanmf/", methods=["POST"])
+def post_classification_result(lang):
+    app.logger.debug('/hitec/classify/concepts/seanmf/ called'.format(lang))
+
+    # app.logger.debug(request.data.decode('utf-8'))
+    content = json.loads(request.data.decode('utf-8'))
+
+    # process content
+
+    # start concept detection
+    #processed_tweets = classification_facade.process_tweets(tweets, lang)
+
+    # app.logger.debug(classified_tweets)
+    #return jsonify(processed_tweets)
+
+    # for now we return the things we received
+    return jsonify(content)
+
+
+if __name__ == "__main__":
+    app.run(debug=False, threaded=False, host=CONFIG['HOST'], port=CONFIG['PORT'])
